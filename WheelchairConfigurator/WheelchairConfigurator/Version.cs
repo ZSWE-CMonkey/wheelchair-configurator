f1ff3ab06d1bc952bc48389fb0aafff934936c07
KristianKamenik <148895436+KristianKamenik@users.noreply.github.com>
2026-03-14 01:26:41 +0100

    added version

---
+++ WheelchairConfigurator/WheelchairConfigurator/Version.cs
@@ -0,0 +1,10 @@
+﻿namespace WheelchairConfigurator
+{
+    public static class Version
+    {
+        public static int Major => 0;
+        public static int Minor => 0;
+        public static int Patch => 1;
+        public static ulong GetVersionNumber64bit() => (((ulong)Major & 0xffffUL) << 48) | (((ulong)Minor & 0xffffUL) << 32) | ((ulong)Patch & 0xffffffffUL);
+    }
+}
