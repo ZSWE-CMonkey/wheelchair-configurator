df09d1290731e74a61d9d04b7bd7ae2c7a1e336f
KristianKamenik <kristian.c.kamenik@gmail.com>
2026-04-03 17:12:46 +0200

    added missing reference

--- WheelchairConfigurator/ConfigurationLogic/Graphics/ImageCompositorGraphicsPluginImpl.cs
+++ WheelchairConfigurator/ConfigurationLogic/Graphics/ImageCompositorGraphicsPluginImpl.cs
@@ -1,12 +1,4 @@
-﻿using Android.Hardware.Lights;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Android.Icu.Text.ListFormatter;
-
-namespace ConfigurationLogic.Graphics
+﻿namespace ConfigurationLogic.Graphics
 {
     internal class ImageCompositorGraphicsPluginImpl : IGraphicsPlugin
     {
