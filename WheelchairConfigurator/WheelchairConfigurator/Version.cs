4b7c2282ce10814327eda730ee242117f099dc7b
KristianKamenik <kristian.c.kamenik@gmail.com>
2026-04-03 17:13:28 +0200

    changed version to 0.4.1.

--- WheelchairConfigurator/WheelchairConfigurator/Version.cs
+++ WheelchairConfigurator/WheelchairConfigurator/Version.cs
@@ -4,7 +4,7 @@
     {
         public static int Major => 0;
         public static int Minor => 4;
-        public static int Patch => 0;
+        public static int Patch => 1;
         public static ulong GetVersionNumber64bit() => (((ulong)Major & 0xffffUL) << 48) | (((ulong)Minor & 0xffffUL) << 32) | ((ulong)Patch & 0xffffffffUL);
     }
 }
