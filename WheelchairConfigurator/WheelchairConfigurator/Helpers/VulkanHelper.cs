6d069cfc54a5be31713f889b1aba2ed946e2ba45
KristianKamenik <kristian.c.kamenik@gmail.com>
2026-04-07 22:47:16 +0200

    MAUI project integration and added Helper

---
+++ WheelchairConfigurator/WheelchairConfigurator/Helpers/VulkanHelper.cs
@@ -0,0 +1,66 @@
+﻿using ConfigurationLogic.Graphics;
+
+namespace WheelchairConfigurator.Helpers
+{
+    /// <summary>
+    /// Vulkan helper for single render of image lol
+    /// </summary>
+    internal class VulkanHelper
+    {
+        private string _name;
+        private int _width;
+        private int _height;
+
+        private List<string> _objectsId;
+
+        private IGraphicsPlugin _graphicsPlugin;
+
+        public VulkanHelper(string name, int widht, int height)
+        {
+            _name = name;
+            _width = widht;
+            _height = height;
+            _objectsId = new List<string>();
+            _graphicsPlugin = GraphicsPluginFactory.CreateVulkanGraphicsPlugin(name, widht, height);
+        }
+
+        /// <summary>
+        /// Adds object to the rendered scene. You must add it first before rendering call skibidi
+        /// </summary>
+        /// <param name="name">Object id name in format like (without any extensions): [subfolder]/[name]</param>
+        public void AddObject(string name)
+        {
+            _objectsId.Add(name);
+        }
+
+        public void ClearObjects()
+        {
+            _objectsId.Clear();
+        }
+
+        public void ChangeWidthHeight(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        /// <summary>
+        /// Initialize and renders once the scene and deinitialize vulkan engine. 
+        /// Outputs final image of that rendering.
+        /// You must add object first before this call :3
+        /// </summary>
+        /// <param name="bytes">Raw image data</param>
+        public void GetRenderedImageBuffer(out byte[] bytes)
+        {
+            foreach(string id in _objectsId)
+            {
+                _graphicsPlugin.AddResource(id);
+            }
+
+            _graphicsPlugin.Initialize();
+            _graphicsPlugin.Render(out bytes);
+            _graphicsPlugin.Deinitialize();
+        }
+
+    }
+}
