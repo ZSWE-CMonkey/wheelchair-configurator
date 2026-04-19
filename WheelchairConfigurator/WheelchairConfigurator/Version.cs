0756e5f465072f1b2237c5cc72cab891c362e0f1
KristianKamenik <148895436+KristianKamenik@users.noreply.github.com>
2026-04-20 01:10:27 +0200

    changed version to 0.9.0.

--- WheelchairConfigurator/WheelchairConfigurator/Version.cs
+++ WheelchairConfigurator/WheelchairConfigurator/Version.cs
@@ -3,7 +3,7 @@
     public static class Version
     {
         public static int Major => 0;
-        public static int Minor => 8;
+        public static int Minor => 9;
         public static int Patch => 0;
         public static ulong GetVersionNumber64bit() => (((ulong)Major & 0xffffUL) << 48) | (((ulong)Minor & 0xffffUL) << 32) | ((ulong)Patch & 0xffffffffUL);
     }
